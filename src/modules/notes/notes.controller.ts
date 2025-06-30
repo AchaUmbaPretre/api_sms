9be6128a2874f3a9f235e4d3611c56a9cafc840e
AchaUmbaPretre <[achandambi@gmail.com]>
2025-06-30 13:57:32 +0200

    commit

---
+++ src/modules/notes/notes.controller.ts
@@ -0,0 +1,58 @@
+import { Request, Response, NextFunction } from "express";
+import { notesService } from "./notes.service";
+import { parse } from "dotenv";
+
+export const notesController = {
+    async create(req:Request, res:Response, next: NextFunction) : Promise<void> {
+        try {
+            await notesService.createdNotes(req.body);
+            res.status(201).json({
+                message: 'La note a été enregistée avec succes'
+            })
+        } catch (error) {
+            next(error)
+        }
+    },
+
+    async findAll(req: Request, res: Response, next: NextFunction) : Promise<void> {
+        try {
+            const note = await notesService.findAll();
+            res.status(200).json(note)
+        } catch (error) {
+            next()
+        }
+    },
+
+    async findById(req: Request, res: Response, next: NextFunction) : Promise<void> {
+        try {
+            const id = parseInt(req.params.id_note);
+            const note = await notesService.findById(id);
+            res.status(200).json(note);
+
+        } catch (error) {
+            next(error);
+        }
+    },
+
+    async  update(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const id = parseInt(req.params.id_note);
+            const updated = await notesService.update(id, req.body);
+            res.status(200).json({
+                message: 'Note mise à jour',
+                data: updated,
+            });
+        } catch (error) {
+            next(error);
+        }
+    },
+      async delete(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+          const id = parseInt(req.params.id_note);
+          await notesService.delete(id);
+          res.status(200).json({ message: 'Note supprimée' });
+        } catch (error) {
+          next(error);
+        }
+      },
+}
---
+++ src/modules/notes/notes.service.ts
@@ -0,0 +1,97 @@
+import { prisma } from '../../config/prisma';
+
+interface CreateNoteDTO {
+    id_note: number;
+    id_eleve: number;
+    id_matiere: number;
+    id_periode: number;
+    note: number
+}
+
+interface CreatePeriodeDTO {
+    nom_periode: string
+}
+
+export const notesService = {
+    async createdNotes(data: CreateNoteDTO) {
+        const {
+            id_eleve,
+            id_matiere,
+            id_periode,
+            note
+        } = data;
+
+        try {
+            return await prisma.notes.create({
+                data: {
+                    id_eleve,
+                    id_matiere,
+                    id_periode,
+                    note
+                }
+            });
+        } catch (error) {
+            console.error('[noteService][createNoteService] Erreur :', error);
+            throw new Error("Impossible de créer une note");
+        }
+    },
+
+    async findAll() {
+        return await prisma.classe.findMany()
+    },
+
+    async findById(id_note: number) {
+        const note = await prisma.notes.findUnique({
+            where: { id_note }
+        });
+
+        if(!note) {
+            throw new Error('Note introuvable');
+        }
+        return note;
+    },
+
+    async update(id_note: number, data: CreateNoteDTO) {
+        try {
+            return await prisma.notes.update({
+                where: { id_note },
+                data
+            })
+        } catch (error) {
+            console.error('[NoteService][update] Erreur :', error);
+            throw new Error("Impossible de mettre à jour la note");
+        }
+    },
+
+    async delete(id_note: number) {
+        try {
+            const note = await prisma.notes.delete({ where: {id_note} })
+        } catch (error) {
+            console.error('[NoteService][delete] Erreur :', error);
+            throw new Error("Impossible de supprimer cette note");
+        }
+    }
+}
+
+export const periodeService = {
+    async createdNotes(data: CreatePeriodeDTO) {
+        const {
+            nom_periode
+        } = data;
+
+        try {
+            return await prisma.periodes.create({
+                data: {
+                    nom_periode
+                }
+            });
+        } catch (error) {
+            console.error('[periodeService] Erreur :', error);
+            throw new Error("Impossible de créer une note");
+        }
+    },
+
+    async findAll() {
+        return await prisma.periodes.findMany()
+    }
+}
